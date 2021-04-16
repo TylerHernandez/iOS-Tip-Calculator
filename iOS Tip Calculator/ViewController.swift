2889781e02f393bda3f3d8975d36e86071d9b0c9
Tyler Hernandez <tylerh6201@gmail.com>
2021-04-16 19:12:42 -0400

    adds no tip as default option; changes layout to take up more of the page

--- iOS Tip Calculator/ViewController.swift
+++ iOS Tip Calculator/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
         let bill = Double(billAmountTextField.text!) ?? 0
         
         // Get Total tip by multiplying tip * tipPercentage
-        let tipPercentages = [0.15, 0.18, 0.2]
+        let tipPercentages = [0.0, 0.15, 0.18, 0.2]
         let tip = bill *
             tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
@@ -40,9 +40,13 @@ class ViewController: UIViewController {
         
         
         // Control background color based on tip % choice
-        if tipControl.selectedSegmentIndex == 0 { self.view.backgroundColor = #colorLiteral(red: 0.6397068776, green: 0.3619226988, blue: 0, alpha: 1)
+        if tipControl.selectedSegmentIndex == 0 {
+            self.view.backgroundColor = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
         }
         else if tipControl.selectedSegmentIndex == 1 {
+            self.view.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        }
+        else if tipControl.selectedSegmentIndex == 2 {
             self.view.backgroundColor = #colorLiteral(red: 0.6397068776, green: 0.6924797629, blue: 0, alpha: 1)
         }
         else {
